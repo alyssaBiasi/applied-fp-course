3234919bd8a003f4e23753a503919c7bb463ae77
Alyssa Biasi <biasi91@gmail.com>
2018-06-22 11:19:59 +1000

    Level 01 - hello world

--- src/Level01/Core.hs
+++ src/Level01/Core.hs
@@ -28,12 +28,8 @@ import           Network.HTTP.Types       (status200)
 -- what you need.
 --
 -- We've used the non-synonym version of the `Application` type below.
-app
-  :: Request
-  -> (Response -> IO ResponseReceived)
-  -> IO ResponseReceived
-app _ cb =
-  error "Application not implemented"
+app :: Request -> (Response -> IO ResponseReceived) -> IO ResponseReceived
+app _ respond = respond $ responseLBS status200 [("Content-Type", "text/plain")] "Hello, Web!"
 
 -- We keep this main function here as it is useful to build your application as
 -- a library. The reasoning behind this is that when you come to do your
@@ -41,4 +37,4 @@ app _ cb =
 -- needing to worry about any initialisation code you've buried in your
 -- executable Main.hs.
 runApp :: IO ()
-runApp = run undefined undefined
+runApp = run 8080 app
